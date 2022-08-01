d95cf23e77f6db786aae93d0d84583e37ec35643
sanghyuk1 <97418790+sanghyuk1@users.noreply.github.com>
2022-08-01 11:00:08 +0900

    lee sung ho

--- lib/dino_game.dart
+++ lib/dino_game.dart
@@ -1,19 +1,20 @@
 import 'package:flame/game.dart';
 import 'dino_player.dart';
 import 'dino_world.dart';
+import 'dart:ui';
 
 class DinoGame extends FlameGame{
 
   DinoPlayer _dinoPlayer = DinoPlayer();
-  DinoWorld _dionWorld = DinoWorld();
+  DinoWorld _dinoWorld = DinoWorld();
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    await add(_dinoPlayer);
+    await add(_dinoPlayer); // 플레이어를 더한다.
     await add(_dinoWorld);
-    _dinoPlayer.position = _dinoWorld.size/1.5;
+    _dinoPlayer.position = _dinoWorld.size/1.5; // 세계 사이즈
     camera.followComponent(_dinoPlayer,
-        worldBounds: Rect.fromLTRB(0, 0, _dinoWorld.size.x, _dinoWorld.size.y));
+        worldBounds: Rect.fromLTRB(0, 0, _dinoWorld.size.x, _dinoWorld.size.y)); // 카메라 따라다니기
   }
 }
--- lib/dino_player.dart
+++ lib/dino_player.dart
@@ -1,1 +1,17 @@
-class DinoPlayer extends
+import 'package:flame/components.dart';
+
+class DinoPlayer extends SpriteComponent with HasGameRef {
+
+DinoPlayer()
+     : super(
+         size: Vector2.all(100.0),
+       );
+
+  @override
+  Future<void> onLoad() async {
+  super.onLoad();
+  sprite = await gameRef.loadSprite("idle.png"); // 공룡 불러오기
+  position = gameRef.size / 2; // 플레이어를 중앙에 위치
+size = sprite!.originalSize; // 배경을 입력할 때 사용
+  }
+}
